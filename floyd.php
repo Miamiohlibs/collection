9688d61d412f020cd7190bf8f0cbde0a99fa8613
craigboman <craig.boman@gmail.com>
2018-09-23 00:16:37 -0400

    not sure whats going on here

--- floyd.php
+++ floyd.php
@@ -1,6 +1,5 @@
 <?php
 
-
 //reset all variables needed for our connection
 $username = null;
 $password = null;
@@ -10,64 +9,87 @@ $connection = null;
 require_once('sierra_cred.php');
 
 //make our database connection
-	$connection = pg_connect($dsn, $username, $password) or die("Could not connect");
-
 
 //set output to utf-8
 $connection->query('SET NAMES UNICODE');
 
 $sql = '
 SELECT
---*
---Nate wants call #, title, last check-in, total checkouts
-p.call_number, b.best_title, i.last_checkin_gmt::date, i.checkout_total
+-- i.inventory_gmt,
+lower(p.barcode) as barcode,
+upper(p.call_number_norm || COALESCE(' ' || v.field_content, '') ) as call_number_norm,
+b.best_title AS best_title,
+i.location_code AS location,
+i.item_status_code AS status,
+s.content AS inventory_note,
+to_timestamp(c.due_gmt::text, 'YYYY-MM-DD HH24:MI:SS') as due_gmt --some dates may require 24 hour time stamp; idk
+
 FROM
-sierra_view.item_record AS i
+sierra_view.item_record_property	AS p
 JOIN
-sierra_view.item_record_property AS p
+sierra_view.item_record			AS i
 ON
-p.item_record_id = i.record_id
-JOIN
-sierra_view.record_metadata AS m
+  p.item_record_id = i.id
+
+LEFT OUTER JOIN
+sierra_view.subfield			AS s
 ON
-m.id = i.record_id
-JOIN
+  (s.record_id = p.item_record_id) AND s.field_type_code = 'w'
+
+LEFT OUTER JOIN
+sierra_view.checkout			AS c
+ON
+  (i.record_id = c.item_record_id)
+
+LEFT OUTER JOIN
+sierra_view.varfield			AS v
+ON
+  i.id = v.record_id AND v.varfield_type_code = 'v'
+
+LEFT JOIN
 sierra_view.bib_record_item_record_link AS l
 ON
-l.item_record_id = i.record_id
-JOIN
+  l.item_record_id = i.id
+
+LEFT JOIN
 sierra_view.bib_record_property as b
 ON
-b.bib_record_id = l.bib_record_id
+  b.bib_record_id = l.bib_record_id
+
 WHERE
-p.call_number_norm 
-BETWEEN 'pn 1000' and 'pn 6000'
-AND
-m.creation_date_gmt::date < CURRENT_DATE - INTERVAL '10 YEARS'
-AND
-i.last_checkout_gmt::date < CURRENT_DATE - INTERVAL '10 YEARS'
-AND
-i.location_code = 'kngli'
-AND
-i.icode2 = '-'
-AND
-m.campus_code = ''
+i.location_code = 'imje'
+--   --comment out this section for items organized by title
+-- AND
+-- p.call_number_norm >= lower('BF   77 U53 1992 SE')
+-- AND
+-- p.call_number_norm <= lower('TX  335 L69 1990 TR BOOK')
 
---ORDER BY p.call_number_norm ASC
-';
 
-$statement = pg_query($connection, $sql);
 
-$row = pg_fetch_result($statement, 1, 0);
+order by
+p.call_number_norm ASC,
+l.items_display_order ASC
 
+--LIMIT 10000
+
+
+'
+;
+
+$statement = $connection->prepare($sql);
+$statement->execute();
+$return_array = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$encode_array['data'] = $return_array;
+$encode_array['query'] = $sql;
+
+//$row['query'] = $sql;
 
 header('Content-Type: application/json');
-echo json_encode($row);
+echo json_encode($encode_array, JSON_PRETTY_PRINT);
 
 $row = null;
 $statement = null;
 $connection = null;
-
-pg_close($connection);
 
 ?>
