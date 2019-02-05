92c1059be1350cddf09ab349c34d8d3391b0bff3
craigboman <craig.boman@gmail.com>
2019-02-05 16:17:10 -0500

    location sql update

--- locations.php
+++ locations.php
@@ -11,13 +11,21 @@ $db_handle = getdb(); //call the db function
 
 $query = "
 SELECT
-l.code
+--*
+l.code,
+n.name
 FROM
 sierra_view.location AS l
+JOIN
+sierra_view.location_name AS n
+ON
+n.location_id = l.id
 
 --LIMIT 10
 
 ORDER BY l.code ASC
+
+;
 ";
 
 
