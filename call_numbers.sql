9acc60bac7e142ae1d6e2c74fc94c639002e48a3
craigboman <craig.boman@gmail.com>
2019-03-12 11:15:58 -0400

    added left function to remove extra and help distinct work better

--- call_numbers.sql
+++ call_numbers.sql
@@ -1,10 +1,22 @@
-﻿SELECT
+﻿--exclude sudoc (086 or field tag), sword (anything starting with 8)
+
+SELECT
 --Distinct *
-DISTINCT p.call_number_norm
+DISTINCT left(p.call_number_norm,7) AS left_call
 FROM
 sierra_view.item_record_property AS p
+
+JOIN
+sierra_view.record_metadata AS m
+ON
+m.id = p.item_record_id
+
 WHERE
 p.call_number_norm != ''
+AND
+m.campus_code = ''
 
---LIMIT 10 -- test 
-ORDER BY p.call_number_norm ASC --production
+
+--OFFSET 17000 -- test 
+--limit 1000
+ORDER BY left_call ASC --production 
